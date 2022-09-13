18baaa2bccb9b1b4d98963dc6e492a9e64479980
Arian salmanzadeh <salmanzadeharian@gmail.com>
2022-09-13 18:32:14 +0430

    completed

---
+++ script.js
@@ -0,0 +1,45 @@
+const nav = document.querySelector(".nav-icon");
+const menuClose = document.querySelector(".menu-close");
+const aboutLines = document.querySelectorAll(".content-line");
+const aboutSection = document.querySelector(".about");
+// const
+
+const loadingFinished = function () {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+    document.querySelector(".loading-effect").classList.add("display");
+  }, 4000);
+};
+
+loadingFinished();
+const toggleMenu = function () {
+  if (nav.nextElementSibling.getAttribute("display").includes("off")) {
+    console.log(nav.getBoundingClientRect().right);
+    nav.nextElementSibling.style.right = "0";
+    nav.nextElementSibling.setAttribute("display", "on");
+    console.log(nav.getBoundingClientRect().width);
+    nav.style.right = `${
+      nav.nextElementSibling.getBoundingClientRect().width -
+      nav.getBoundingClientRect().width
+    }px`;
+  } else {
+    nav.nextElementSibling.style.right = "-100%";
+    nav.nextElementSibling.setAttribute("display", "off");
+    nav.style.right = `${nav.getBoundingClientRect().width}px`;
+  }
+};
+const aboutObject = {
+  root: null,
+  rootMargin: "-300px",
+};
+const aboutFallback = function (aboutObject, observer) {
+  console.log(aboutLines);
+  const [options] = aboutObject;
+  if (!options.isIntersecting) return;
+  aboutLines.forEach((elem) => (elem.style.width = "9vw"));
+};
+
+menuClose.addEventListener("click", toggleMenu);
+nav.addEventListener("click", toggleMenu);
+aboutObserver = new IntersectionObserver(aboutFallback, aboutObject);
+aboutObserver.observe(aboutSection);
